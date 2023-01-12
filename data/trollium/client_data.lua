3ccae31bf4164e3d566b7b42933505ed7bbfaba2
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-01-12 08:56:05 -0500

    Create client_data.lua

---
+++ data/trollium/client_data.lua
@@ -0,0 +1,15 @@
+return {
+    version = "v0.1.5",
+    support = "Univsersal",
+    changelogs = {},
+    features = {
+        "Invisiblity",
+        "Carfucker"
+    },
+    LookFor = {
+        rnet = function() if getgenv().rnet then return true else return false end end,
+        debugsetcode = function() if getgenv().debug.setcode then return true else return false end end,
+        debuggetcode = function() if getgenv().debug.getcode then return true else return false end end,
+        sendphysics = function() if getgenv().rnet.sendphysics then return true else return false end end
+    }
+}
